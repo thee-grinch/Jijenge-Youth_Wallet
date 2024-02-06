16ed65e48c5dfbb00c7544a76c639c8430ae1259
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-02-06 19:07:30 +0300

    create user route working properly

--- app/api/users.py
+++ app/api/users.py
@@ -1,20 +1,23 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlmodel import Session
 
 from sql.models import User
 from sql.database import get_db
-from utils.utils import hash_pass
-from utils.user_verification import create_link
+from utils.utils import hash_pass, verify_password
+from utils.user_verification import create_link, decode_token
 from utils.send_mail import send_mail
 from schemas.user import *
+from utils.oauth import create_access_token
+from schemas.schemas import *
 
 router = APIRouter()
 
 @router.post('/new/')
 def create_user(new_user: CreateUser, db: Session = Depends(get_db)):
-    """creates a new user to the database
-        this route is responsible for creating a new user,
-        Arguments:
+    """
+        creates a new user to the database
+        Parameters:
             new_user: an instance of createUser class a schema with the necessary fields
             db: the session class, which represents the database which the user will be saved
         functioning:
@@ -34,3 +37,23 @@ def create_user(new_user: CreateUser, db: Session = Depends(get_db)):
     link = create_link(new_user.email)
     send_mail(new_user.email, link, new_user.first_name)
     return {'message': "{} was created successfully".format(new_user.first_name)}
+
+@router.get('/verify')
+def verify_user(token: str, db: Session = Depends(get_db)):
+    email = decode_token(token)
+    user = db.query(User).filter(User.email == email).first()
+    user.is_verified = True
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+@router.post('/login', response_model=Token)
+def login(userdetails: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.user_name == userdetails.username).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"The user Does Not Exist")
+    if not verify_password(userdetails.password, user.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"The passwords do not match")
+    access_token = create_access_token(data={"user_id": user.user_id})
+    return {"access_token": access_token, "token_type": "bearer"}
--- app/app.py
+++ app/app.py
@@ -1,18 +1,18 @@
-from fastapi import FastAPI, Depends,  HTTPException, status,APIRouter
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import FastAPI #, Depends,  HTTPException, status,APIRouter
+# from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.middleware.cors import CORSMiddleware
 
-from sqlmodel import Session
-from datetime import datetime
+# from sqlmodel import Session
+# from datetime import datetime
 
-from utils.oauth import create_access_token
+# from utils.oauth import create_access_token
 from sql.database import create_db_and_tables, engine, get_db
-from sql.models import User, Contribution, Loan, LoanType, Administrator, Saving
-from utils.utils import hash_pass, verify_password
-from utils.user_verification import create_link, decode_token
-from utils.send_mail import send_mail
+# from sql.models import User, Contribution, Loan, LoanType, Administrator, Saving
+# from utils.utils import hash_pass, verify_password
+# from utils.user_verification import create_link, decode_token
+# from utils.send_mail import send_mail
 from api import users
-from schemas import schemas
+# from schemas import schemas
 # from sql.dbfunctions import create_user
 # routers = APIRouter(
 #     tags=['Authentication']
@@ -61,25 +61,25 @@ def main():
 #     link = create_link(new_user.email)
 #     send_mail(new_user.email, link, new_user.first_name)
 #     return {'message': "{} was created successfully".format(new_user.first_name)}
-@app.get('/verify')
-def verify_user(token: str, db: Session = Depends(get_db)):
-    email = decode_token(token)
-    user = db.query(User).filter(User.email == email).first()
-    user.is_verified = True
-    db.add(user)
-    db.commit()
-    db.refresh(user)
-    return {'message': 'email verified successfully'}
-@app.post('/login', response_model=schemas.Token)
-def login(userdetails: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.user_name == userdetails.username).first()
+# @app.get('/verify')
+# def verify_user(token: str, db: Session = Depends(get_db)):
+#     email = decode_token(token)
+#     user = db.query(User).filter(User.email == email).first()
+#     user.is_verified = True
+#     db.add(user)
+#     db.commit()
+#     db.refresh(user)
+#     return {'message': 'email verified successfully'}
+# @app.post('/login', response_model=schemas.Token)
+# def login(userdetails: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+#     user = db.query(User).filter(User.user_name == userdetails.username).first()
 
-    if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"The user Does Not Exist")
-    if not verify_password(userdetails.password, user.password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"The passwords do not match")
-    access_token = create_access_token(data={"user_id": user.user_id})
-    return {"access_token": access_token, "token_type": "bearer"}
-@app.post('loan')
-def create_loan(new_loan: Loan):
+#     if not user:
+#         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"The user Does Not Exist")
+#     if not verify_password(userdetails.password, user.password):
+#         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"The passwords do not match")
+#     access_token = create_access_token(data={"user_id": user.user_id})
+#     return {"access_token": access_token, "token_type": "bearer"}
+# @app.post('loan')
+# def create_loan(new_loan: Loan):
     pass

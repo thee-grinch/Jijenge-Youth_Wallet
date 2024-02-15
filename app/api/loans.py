2a20b79b52102267c320a42b6a56a98abf39102d
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-02-15 17:20:45 +0300

    Update loan creation endpoint and logic.

--- app/api/loans.py
+++ app/api/loans.py
@@ -6,6 +6,7 @@ from fastapi import Depends, APIRouter
 from sqlalchemy.orm import session
 
 from utils.oauth import get_current_user
+from sql.models import User
 from sql.database_alchemy import get_db
 from schemas.loans import Loan, loanCreate
 from utils.loans import new_loan
@@ -14,3 +15,9 @@ router = APIRouter()
 @router.post('/new_loan')
 def create_loan(loan: loanCreate, user_id = Depends(get_current_user), db: session = Depends(get_db)):
     return new_loan(loan, user_id, db)
+
+@router.get('/loan_status')
+def get_status(user_id = Depends(get_current_user), db: session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    return {'message': user.loans[0].status}
+

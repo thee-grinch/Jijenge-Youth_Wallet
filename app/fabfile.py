cb0c7313e5875e0641dbea50fa317fdd4fe1cc0b
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-03-08 14:28:30 +0300

    update requirements.txt

--- app/fabfile.py
+++ app/fabfile.py
@@ -1,5 +1,6 @@
 from fabric import Connection, task
 
+
 page = '''
 <!DOCTYPE html>
 <html lang="en">
@@ -48,7 +49,7 @@ page = '''
 
 '''
 conn_kwargs = {
-    "host": "54.88.227.197",
+    "host": "18.207.142.2",
     "user": "ubuntu",
     "connect_kwargs": {
         "key_filename": '/home/mordecai/.ssh/id_rsa',
@@ -61,17 +62,22 @@ server {
     server_name www.jijenge.muvandii.tech jijenge.muvandii.tech;
     root /var/jijenge;
     index index.html index.htm index.nginx-debian.html;
-    # location / {
-    #     try 
-    # }
+    location /app {
+        proxy_pass http://localhost:8000;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+
+    }
 }
 '''
 
 @task
 def recreate(c):
     conn = Connection(**conn_kwargs)
-    conn.run('sudo rm /etc/nginx/sites-enabled/jijenge')
-    conn.run('sudo rm /etc/nginx/sites-available/jijenge')
+    # conn.run('sudo rm /etc/nginx/sites-enabled/jijenge')
+    # conn.run('sudo rm /etc/nginx/sites-available/jijenge')
     conn.run('sudo touch /etc/nginx/sites-available/jijenge')
     conn.run(f'echo "{my_server_block}" | sudo tee /etc/nginx/sites-available/jijenge')
     conn.run('sudo ln -s /etc/nginx/sites-available/jijenge /etc/nginx/sites-enabled')
@@ -81,20 +87,46 @@ def recreate(c):
 @task
 def add(c):
     conn = Connection(**conn_kwargs)
-    # conn.run('sudo mkdir /var/jijenge')
+    conn.run('sudo mkdir -p /var/jijenge')
     conn.run(f'echo "{page}" | sudo tee /var/jijenge/index.html')
 @task
 def list(c):
     conn = Connection(**conn_kwargs)
-    conn.run('ls /var/jijenge')
+    conn.run('pwd')
+    conn.run('ls ~/Jijenge-Youth_Wallet')
+    # conn.run('ls /var/jijenge')
 
 @task
 def install(c):
     conn = Connection(**conn_kwargs)
     conn.run('sudo apt update')
-    conn.run('sudo apt install pip3')
+    conn.run('sudo apt install --upgrade python3')
+    conn.run('sudo apt install python3-pip')
 
 @task
 def clone(c):
     conn = Connection(**conn_kwargs)
-    conn.run('git clone
+    conn.run('sudo rm -rf Jijenge-Youth_Wallet')
+    result = conn.run('git clone https://github.com/thee-grinch/Jijenge-Youth_Wallet.git')
+    if result.failed:
+        conn.run('git -C Jijenge-Youth_Wallet pull')
+    else:
+         conn.run('cd Jijenge-Youth_Wallet && cd app && pip install -r requirements.txt && uvicorn app:app --reload')
+
+@task
+def check(c):
+    conn = Connection(**conn_kwargs)
+    conn.run('ps aux | grep uvicorn')
+    # conn.run('sudo service nginx restart')
+@task
+def run(c):
+    conn = Connection(**conn_kwargs)
+    conn.run('cd Jijenge-Youth_Wallet && cd app && pip3 install -r requirements.txt && uvicorn app:app --reload')
+    conn.run('sudo nginx -t')
+    conn.run('sudo service nginx restart')
+
+@task
+def nginx(c):
+    conn = Connection(**conn_kwargs)
+    conn.run('sudo apt update')
+    conn.run('sudo apt install nginx')
